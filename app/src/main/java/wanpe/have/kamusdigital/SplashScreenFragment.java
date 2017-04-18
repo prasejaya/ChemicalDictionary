d59c23f3feecf32437ce26c54f61188da5284af3
Edo Prasetyo <prasejaya@gmail.com>
2017-04-18 17:25:10 +0700

    start awal

---
+++ app/src/main/java/wanpe/have/kamusdigital/SplashScreenFragment.java
@@ -0,0 +1,22 @@
+package wanpe.have.kamusdigital;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SplashScreenFragment extends Fragment {
+
+    public SplashScreenFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false);
+    }
+}
